ad1313113375f489eafebc1e670be3faf09e3ef1
bigDataKJ <kang1783922>
2022-03-09 17:14:05 +0800

    test

---
+++ flink-study/src/main/java/com/study/flink/main/MainStart.java
@@ -0,0 +1,13 @@
+package com.study.flink.main;
+
+/**
+ * @Description:
+ * @Author: kangjiang
+ * @Date: 2022/3/9 14:56
+ * @Version: 1.0
+ */
+public class MainStart {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
